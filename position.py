fdb325083d89e3a4d599d231f564f0c5c8ea162a
xdongz <454843286@qq.com>
2018-01-04 11:38:28 +0800

    Add some annotation

--- position.py
+++ position.py
@@ -1,16 +1,25 @@
 #coding:utf-8;
 #qpy:2
 #qpy:console
+
+#creator:tongy
+#collaborators: fengx,cuidongd
+#
+#Script locates the Android Phone 
+#
+#
+#-------------------------------------
+#Imports
 import json
 import itchat
 from androidhelper import Android
 import time
-#from urllib.request import urlopen
 from urllib2 import urlopen
 import threading
 import base64
 import random
 import werobot
+#---------------------------------------
 
 #itchat.auto_login(enableCmdQR=True)
 
@@ -18,7 +27,6 @@ def locate():
     d=Android()
     
     d.startLocating()
-    #time.sleep(3)
 
     loc=d.getLastKnownLocation().result['gps']
     if loc==None:
@@ -26,28 +34,19 @@ def locate():
     else:
         print('it is gps')
         gps=loc
-    #print (loc)
     lngx=gps['longitude']
     lngy=gps['latitude']
-
-#ur='http://api.map.baidu.com/ag/coord/convert?from=0&to=4&x='+str(lngx)+'&y='+str(lngy)
+ 
+    #Get longitude and latitude in Tencent Map
     ur='http://apis.map.qq.com/ws/coord/v1/translate?locations='+str(lngy)+','+str(lngx)+'&type=1&key=Y5YBZ-VLZCX-EQR4Y-Z47JH-TZCHQ-TXF7H'
     f=urlopen(ur).read()
     f=json.loads(f)
-
-#lngx=base64.b64decode(f['x'].encode())
-#lngy=base64.b64decode(f['y'].encode())
-#lngx=lngx.decode()
-#lngy=lngy.decode()
-
-#url='http://lbs.juhe.cn/api/getaddressbylngb?lngx='+str(lngx)+'&lngy'+str(lngy)
-
-#g=urlopen(url).read()
-#g=json.loads(g.decode())
+	
     pos_lng=f['locations'][0]['lng']
     pos_lat=f['locations'][0]['lat']
     print(pos_lat)
     print(pos_lng)
+	#Latitude and longitude are changed to address
     url='http://apis.map.qq.com/ws/geocoder/v1/?location='+str(pos_lat)+','+str(pos_lng)+'&key=Y5YBZ-VLZCX-EQR4Y-Z47JH-TZCHQ-TXF7H&get_poi=1'
     g=urlopen(url).read()
     g=json.loads(g)
@@ -57,10 +56,9 @@ def locate():
     
     #werobot.sendmess(addr)
 
-#g=json.loads(g.decode())
- #   print(f['locations'][0]['lat'])
-  #  print(f['locations'][0]['lng'])
+    #timer
     global timer
+	#Add random constant to control the frequency
     tt=300+random.uniform(0,30)
     print(tt)
     timer=threading.Timer(tt,locate)
@@ -72,9 +70,7 @@ print ('start '+time.strftime('%H:%M:%S',time.localtime(time.time())))
 time.sleep(600)
 timer.cancel()
 print ('end '+time.strftime('%H:%M:%S',time.localtime(time.time())))
-#print(f)
-#print("This is console module")
-#print(lngx,lngy)
+
 
 
      
